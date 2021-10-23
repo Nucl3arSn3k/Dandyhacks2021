5494550edfa36761a5426ba6c717bcd7aad7109d
Tormenator1 <shredderofwheat@gmail.com>
2021-10-22 23:49:03 -0400

    Import works and qstrings do not

--- Misc/translator.py
+++ Misc/translator.py
@@ -1,9 +1,10 @@
 import http.client
-import PySide6.QtCore
-#qt links: https://doc.qt.io/qtforpython/PySide6/QtCore/QStringConverter.html, https://doc.qt.io/qtforpython/quickstart.html
+from PyQt6 import QtCore
+
+# qt links: https://doc.qt.io/qtforpython/PySide6/QtCore/QStringConverter.html, https://doc.qt.io/qtforpython/quickstart.html
 
 
-# Encode 
+# Encode
 string = "Hello World"
 fromUtf16 = QStringEncoder(QStringEncoder.Utf8)
 encodedString = fromUtf16(string)
@@ -11,16 +12,16 @@ encodedString = fromUtf16(string)
 # request logic
 conn = http.client.HTTPSConnection("google-translate1.p.rapidapi.com")
 
-# string as 
+# string as
 encodedString = "q=Hello%2C%20world!&target=es&source=en"
 #                         target lang ^         ^ base lang
 
-headers = { # all static fields
-    'content-type': "application/x-www-form-urlencoded",
-    'accept-encoding': "application/gzip",
-    'x-rapidapi-key': "4679425f88mshcb1cacb0c2357d8p1e02e8jsn4e69292eb0dc",
-    'x-rapidapi-host': "google-translate1.p.rapidapi.com"
-    }
+headers = {  # all static fields
+    "content-type": "application/x-www-form-urlencoded",
+    "accept-encoding": "application/gzip",
+    "x-rapidapi-key": "4679425f88mshcb1cacb0c2357d8p1e02e8jsn4e69292eb0dc",
+    "x-rapidapi-host": "google-translate1.p.rapidapi.com",
+}
 
 conn.request("POST", "/language/translate/v2", encodedString, headers)
 
