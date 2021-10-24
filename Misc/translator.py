1df56bb2a142ad27155db4e0754b1da6095f3a86
Todo25 <87049757+toraOzawa@users.noreply.github.com>
2021-10-23 21:51:04 -0400

    paragraph parsing and translation

--- Misc/translator.py
+++ Misc/translator.py
@@ -1,59 +1,61 @@
 import http.client
+import re
 import html2text # https://pypi.org/project/html2text/
-from bs4 import BeautifulSoup # https://www.tutorialspoint.com/beautiful_soup/beautiful_soup_installation.htm 
 from PyQt6 import QtCore
 
 
-#
-url = "http://kite.com"
-html = urlopen(url).read()
-soup = BeautifulSoup(html)
+# do below upon page load?
+def translate_html_from(url):
 
-for script in soup(["script", "style"]):
-    script.decompose() # deletes out tags
+    rawHtml = urlopen(url).read()
 
-strips = soup.stripped_strings
+    pList = re.findall("<p>(.*?)</p>", rawHtml)
+
+    resultPList = []
+    for p in pList:
+        resultPList.append(re.sub(" +", " ", re.sub("<.*?>", " ", p))) 
+
+    finalPList = []
+    for p in resultPList:
+        finalPList.append(translate(p))
+
+    return finalPList
+
+
+
 
 # qt links: https://doc.qt.io/qtforpython/PySide6/QtCore/QStringConverter.html, https://doc.qt.io/qtforpython/quickstart.html
+def translate(string):
+    resultTargetLang = switchNotation("Spanish")
+    resultSourceLang = switchNotation("English")
+    fromUtf16 = QtCore.QStringEncoder(QtCore.QStringEncoder.Encoding.Utf8)
+    encodedString = fromUtf16(string)
+
+    # request logic
+    conn = http.client.HTTPSConnection("google-translate1.p.rapidapi.com")
+
+    # string as
+    encodedString = "q=Hello%2C%20world!&target=" + resultTargetLang + "&source=" + resultSourceLang
+
+    headers = {  # all static fields
+        "content-type": "application/x-www-form-urlencoded",
+        "accept-encoding": "application/gzip",
+        "x-rapidapi-key": "4679425f88mshcb1cacb0c2357d8p1e02e8jsn4e69292eb0dc",
+        "x-rapidapi-host": "google-translate1.p.rapidapi.com",
+    }
+
+    conn.request("POST", "/language/translate/v2", encodedString, headers)
+
+    res = conn.getresponse()
+    data = res.read()
+    return data.decode("utf-8")
 
 def switchNotation(argument):
     switcher = {
         "English": "en",
-        "Spanish": "es",
-        "Italian": "it",
-        "French": "fr",
-        "German": "de",
+        "Español": "es",
+        "Italiano": "it",
+        "Français": "fr",
+        "Deutsche": "de",
     }
     return switcher.get(argument, "en")
-
-
-# Encode
-string =  "Hello World"
-targetLanguage = "Spanish"
-sourceLanguage = "English"
-targetLanguage = switchNotation(targetLanguage)
-sourceLanguage = switchNotation(sourceLanguage)
-
-
-fromUtf16 = QtCore.QStringEncoder(QtCore.QStringEncoder.Encoding.Utf8)
-encodedString = fromUtf16(string)
-
-# request logic
-conn = http.client.HTTPSConnection("google-translate1.p.rapidapi.com")
-
-# string as
-encodedString = "q=Hello%2C%20world!&target=" + targetLanguage + "&source=" + sourceLanguage
-
-headers = {  # all static fieldse
-    "content-type": "application/x-www-form-urlencoded",
-    "accept-encoding": "application/gzip",
-    "x-rapidapi-key": "4679425f88mshcb1cacb0c2357d8p1e02e8jsn4e69292eb0dc",
-    "x-rapidapi-host": "google-translate1.p.rapidapi.com",
-}
-
-conn.request("POST", "/language/translate/v2", encodedString, headers)
-
-res = conn.getresponse()
-data = res.read()
-
-print(data.decode("utf-8"))
